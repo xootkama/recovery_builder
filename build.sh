1aa28782fba323222826eb26f007211ec3a66c75
MasterSenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-08-16 03:30:52 +0800

    E6746

--- build.sh
+++ build.sh
@@ -2,12 +2,10 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni -b twrp-10.0"
-KT_LINK="https://github.com/MiCode/Xiaomi_Kernel_OpenSource.git"
-KT_PATH=kernel/xiaomi/$DEVICE
-DEVICE=angelicain
-DT_LINK="https://github.com/mastersenpai05/twrp_device_xiaomi_angelicain"
-DT_PATH=device/xiaomi/$DEVICE
+MANIFEST="https://github.com/SHRP/platform_manifest_twrp_omni -b v3_10.0"
+DEVICE=E6746
+DT_LINK="https://github.com/mastersenpai05/twrp_micromax_e6746 -b shrp-10.0"
+DT_PATH=device/micromax/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -29,12 +27,11 @@ lunch omni_${DEVICE}-eng && mka recoveryimage
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
-version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
-
+#version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
+#OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
 cd out/target/product/$DEVICE
-mv recovery.img ${OUTFILE%.zip}.img
-zip -r9 $OUTFILE ${OUTFILE%.zip}.img
+#mv recovery.img ${OUTFILE%.zip}.img
+#zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 
 #curl -T $OUTFILE https://oshi.at
 curl -sL $OUTFILE https://git.io/file-transfer | sh
