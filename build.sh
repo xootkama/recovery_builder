eabaf0ab707558c3ecb455bbbf930a21aa96a2b5
master <85785239+senpaimaster05@users.noreply.github.com>
2021-08-17 18:10:13 +0800

    star

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@ MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.g
 DEVICE=star
 DT_LINK="https://github.com/mastersenpai05/device_xiaomi_star_twrp"
 DT_PATH=device/xiaomi/$DEVICE
-KT_LINK="https://github.com/MiCode/Xiaomi_Kernel_OpenSource -b star-r-oss"
-KT_PATH=kernel/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -15,7 +13,6 @@ echo " ===+++ Syncing Recovery Sources +++==="
 repo init --depth=1 -u $MANIFEST
 repo sync
 git clone --depth=1 $DT_LINK $DT_PATH
-git clone --depth=1 $KT_LINK $KT_PATH
 
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
