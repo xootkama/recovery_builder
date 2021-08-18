c2e4a07f8b4bde1b70f17b5a492af8da3e522b70
master <85785239+senpaimaster05@users.noreply.github.com>
2021-08-18 18:45:09 +0800

    star

--- build.sh
+++ build.sh
@@ -13,7 +13,6 @@ echo " ===+++ Syncing Recovery Sources +++==="
 repo init --depth=1 -u $MANIFEST
 repo sync
 git clone --depth=1 $DT_LINK $DT_PATH
-git clone --depth=1 $KT_LINK $KT_PATH
 
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
@@ -24,43 +23,6 @@ lunch twrp_${DEVICE}-eng && mka bootimage
 echo " ===+++ Uploading Recovery +++==="
 version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
 OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
-
-cd out/target/product/$DEVICE
-mv boot.img ${OUTFILE%.zip}.img
-zip -r9 $OUTFILE ${OUTFILE%.zip}.img
-
-#curl -T $OUTFILE https://oshi.at
-curl -sL $OUTFILE https://git.io/file-transfer | sh
-./transfer wet *.zip
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git"
-DEVICE=ares
-DT_LINK="https://github.com/mastersenpai05/test_device_xiaomi_ares"
-DT_PATH=device/xiaomi/$DEVICE
-
-echo " ===+++ Setting up Build Environment +++==="
-apt install openssh-server -y
-apt update --fix-missing
-apt install openssh-server -y
-mkdir ~/twrp && cd ~/twrp
-
-echo " ===+++ Syncing Recovery Sources +++==="
-repo init --depth=1 -u $MANIFEST
-repo sync
-git clone --depth=1 $DT_LINK $DT_PATH
-
-echo " ===+++ Building Recovery +++==="
-. build/envsetup.sh
-export ALLOW_MISSING_DEPENDENCIES=true
-lunch twrp_${DEVICE}-eng && mka bootimage
-
-# Upload zips & recovery.img
-echo " ===+++ Uploading Recovery +++==="
-version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
-
-cd out/target/product/$DEVICE
-mv boot.img ${OUTFILE%.zip}.img
-zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 
 #curl -T $OUTFILE https://oshi.at
 curl -sL $OUTFILE https://git.io/file-transfer | sh
