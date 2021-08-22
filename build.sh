48e7ee913a1a340c23d5ea30600bc03a0d24289f
master <85785239+senpaimaster05@users.noreply.github.com>
2021-08-22 13:35:40 +0800

    Hi

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git"
-DEVICE=star
-DT_LINK="https://github.com/mastersenpai05/device_xiaomi_star_twrp"
-DT_PATH=device/xiaomi/$DEVICE
+#ANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git"
+#EVICE=star
+#DT_LINK="https://github.com/mastersenpai05/device_xiaomi_star_twrp"
+#T_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -10,21 +10,13 @@ apt install openssh-server -y
 mkdir ~/twrp && cd ~/twrp
 
 echo " ===+++ Syncing Recovery Sources +++==="
-repo init --depth=1 -u $MANIFEST
-repo sync
-repo sync
-git clone --depth=1 $DT_LINK $DT_PATH
-
-echo " ===+++ Building Recovery +++==="
-. build/envsetup.sh
-export ALLOW_MISSING_DEPENDENCIES=true
-lunch twrp_${DEVICE}-eng && mka bootimage
+wget http://download939.mediafire.com/pqmvbjk59gkg/m81fk38cgxflau2/Infinix_Hot_10S_X689B_MT6768_V181_210326.zip
+unzip x Infinix_Hot_10S_X689B_MT6768_V181_210326.zip
+cd Infinix_Hot_10S_X689B_MT6768_V181_210326
+ls
 
 # Upload zips & recovery.img
 echo " ===+++ Uploading Recovery +++==="
-cd out/target/product/$DEVICE
-version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
 mv boot.img ${OUTFILE%.zip}.img
 zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 
