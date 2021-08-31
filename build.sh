114f1c046a2091be42f2f37e49cd395587f0e463
master <85785239+senpaimaster05@users.noreply.github.com>
2021-08-31 22:33:48 +0800

    RMX2121_10.0

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # Just a basic script U can improvise lateron asper ur need xD 
 
 MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni"
-DEVICE=RMX2111
-DT_LINK="https://github.com/senpaimaster05/twrp_realme_RMX2111 -b android-10.0"
+DEVICE=RMX2121
+DT_LINK="https://github.com/mastersenpai0405/twrp_device_realme_rmx2121 -b android-10.0"
 DT_PATH=device/realme/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
