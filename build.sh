08900189afce96a3a086c96a76ecccb1e816a0fa
master <85785239+senpaimaster05@users.noreply.github.com>
2021-09-01 02:57:00 +0800

    E7746

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="https://gitlab.com/OrangeFox/sync.git"
-DEVICE=dandelion
-DT_LINK="https://github.com/senpaimaster05/twrp_xiaomi_dandelion"
-DT_PATH=device/xiaomi/$DEVICE
+MANIFEST="https://github.com/SHRP/platform_manifest_twrp_omni -b v3_10.0"
+DEVICE=E7746
+DT_LINK="https://github.com/mastersenpai0405/twrp_micromax_in_note_1"
+DT_PATH=device/micromax/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -14,22 +14,21 @@ apt install openssh-server -y
 mkdir ~/twrp && cd ~/twrp
 
 echo " ===+++ Syncing Recovery Sources +++==="
-git clone $MANIFEST ~/work && cd ~/work
-./get_fox_10.sh ~/fox && cd ~/fox
+repo init --depth=1 -u $MANIFEST
+repo sync
 git clone $DT_LINK $DT_PATH
 
 echo " ===+++ Building Recovery +++==="
 . build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export LC_ALL="C"
 lunch omni_${DEVICE}-eng && mka recoveryimage
 
-# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+# Upload zips & recovery.img
 echo " ===+++ Uploading Recovery +++==="
-cd out/target/product/$DEVICE
 #version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
 #OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
+
+cd out/target/product/$DEVICE
 #mv recovery.img ${OUTFILE%.zip}.img
 #zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 
